7fb11d0897b76a556ef1f4304c99b39fa0620e42
Ronney Arias <ronneyarias@gmail.com>
2023-10-21 21:03:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+      return driver.toLowerCase().indexOf(string.toLowerCase()) !== -1;
+    });
+  }
+
+  function fuzzyMatch(drivers, string) {
+    
+    let matches = [];
+  
+    
+    for (let i = 0; i < drivers.length; i++) {
+      
+      if (drivers[i].startsWith(string)) {
+        
+        matches.push(drivers[i]);
+      }
+    }
+  
+    
+    return matches;
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
